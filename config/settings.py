3c2cce87d036038a3c385827d531ccfff99bc2dc
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-04-22 14:30:30 +0900

    Modified setting.py

--- config/settings.py
+++ config/settings.py
@@ -84,11 +84,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,       # 長めの keep-alive
+        ssl_require=True        # Heroku の Postgres は SSL 接続が推奨
+    )
 }
 
 # Password validation
