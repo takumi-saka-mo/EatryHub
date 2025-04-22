bb186eb41df049c23ace065303ba42afa6a62b03
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-04-23 02:19:02 +0900

    update setting.py

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,7 @@ from decouple import config, Csv
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
