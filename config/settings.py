44169f57f4bf00b5f62933a9cc4899be4b58ad1f
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-04-22 16:58:20 +0900

    update setting.py

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_CHARSET = 'utf-8'
