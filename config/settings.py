b74366aa5ca86d2bedbc4ba9326971b4ca68ffd7
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-04-22 14:35:40 +0900

    Modified setting.py

--- config/settings.py
+++ config/settings.py
@@ -89,10 +89,10 @@ import dj_database_url
 
 
 DATABASES = {
-        'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,       # 長めの keep-alive
-        ssl_require=True        # Heroku の Postgres は SSL 接続が推奨
+    'default': dj_database_url.config(
+    default=config('DATABASE_URL'),
+    conn_max_age=600,
+    ssl_require=True
     )
 }
 
