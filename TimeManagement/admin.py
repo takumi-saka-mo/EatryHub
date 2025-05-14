009b8a60b22b504aae645595b1cab50400305412
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-05-15 02:34:34 +0900

    Update admin-page (to fix database of TimeManagement)

--- TimeManagement/admin.py
+++ TimeManagement/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import TimeManagementRecord
 
 CustomUser = get_user_model()
 
@@ -8,3 +9,9 @@ if not admin.site.is_registered(CustomUser):
     @admin.register(CustomUser)
     class CustomUserAdmin(admin.ModelAdmin):
         list_display = ('username', 'email', 'is_staff', 'is_active')
+
+@admin.register(TimeManagementRecord)
+class TimeManagementRecordAdmin(admin.ModelAdmin):
+    list_display = ('date', 'row_number', 'table_number', 'plan_name', 'start_time', 'end_time', 'out_time')
+    list_filter = ('date', 'store')
+    search_fields = ('row_number', 'table_number', 'plan_name')
