70ef1e2eba982ca279d88100120e0ca8484bc703
takumi.saka.mo <takumi.saka.mo0107@gmail.com>
2025-05-27 10:43:37 +0900

    update admin-page

--- EatryHub/urls.py
+++ EatryHub/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import LoginView, LogoutView, index, home, mobile_view
+from django.contrib import admin
+
 
 app_name = 'EatryHub'
+
+admin.site.site_title = '管理画面' 
+admin.site.site_header = 'EatryHub - 管理画面' 
+admin.site.index_title = 'メニュー'
 
 urlpatterns = [
     path('', index, name='index'),
